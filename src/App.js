cdb036af5b8ac1d5035315cbe4de2026a367476d
snobojohan <johan.hallberg@gmail.com>
2018-02-14 00:10:46 +0100

    First play

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="app">
+
+        <header className="app-header">
+          
+          <div className="card1">Menu</div>
+          <div className="card2">LOGO</div>
+          <div className="card3">Cart</div>
+
+          <div className="card4">SEARCH</div>
+
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <section className="app-body">
+          <p className="intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+
+          <p>
+            Lörem ipsüm dölör sit ämet, cönsectetür ädipiscing elit. Mörbi feügiät vestibülüm sem, äc süscipit sem lüctüs qüis. Qüisqüe vel ligülä säpien. Pellentesqüe häbitänt mörbi tristiqüe senectüs et netüs et mälesüädä fämes äc türpis egestäs. Dönec fermentüm dignissim mägnä, eü pösüere qüäm phäreträ nec. Ut erös tellüs, üllämcörper vülpütäte tempör ät, tincidünt qüis mi. Phäsellüs cönsectetür äliqüäm löbörtis. Dönec üt pösüere düi. Aliqüäm erät völütpät. Phäsellüs äüctör cürsüs ärcü, vitäe väriüs mäüris dignissim id. Mäüris üt pürüs enim. Vivämüs viverrä, pürüs tempüs bibendüm löbörtis, erät mässä ädipiscing pürüs, vitäe imperdiet nisi felis hendrerit liberö.
+          </p>
+
+          <p>
+            Mörbi läciniä nisl sed säpien läciniä tincidünt. In id ligülä felis. Aeneän pülvinär, felis nec äüctör pörtä, pürüs felis äliqüet läcüs, et cürsüs läcüs mi eget leö. Cüräbitür eüismöd pülvinär metüs qüis cömmödö. Ut semper äliqüet cönvällis. Nüllä vel mägnä id nisl mälesüädä tempör qüis sed liberö. Nülläm pläcerät düi üt diäm äliqüäm äc scelerisqüe nüllä söllicitüdin. Vestibülüm tristiqüe, änte in ädipiscing mälesüädä, nibh metüs äliqüet läcüs, äc mälesüädä äügüe lörem sit ämet est. Cüräbitür qüäm neqüe, egestäs eü väriüs vel, bländit eü elit. Sed grävidä erös äügüe. Integer cöndimentüm pellentesqüe möllis.
+          </p>
+
+          <p>
+            Süspendisse vel törtör ä est pellentesqüe mölestie. Präesent sed nüllä jüstö, in läciniä nibh. Pellentesqüe qüis diäm mäüris, id lüctüs nünc. Präesent eleifend velit ä ödiö eleifend üt äliqüäm mi üllämcörper. Nünc eü mäüris säpien. In qüis äügüe mässä, in läöreet mäüris. Vivämüs ligülä risüs, interdüm eü semper id, iäcülis sed säpien. Ut pellentesqüe erät äc ipsüm fermentüm eü imperdiet nisi tincidünt. Nünc cömmödö cöngüe metüs bländit cönsectetür. Integer leö läcüs, ültricies et cönsectetür ät, fäücibüs sit ämet törtör. Integer vel ipsüm eü nisi mölestie tempüs. Präesent sit ämet qüäm ligülä. Aeneän äügüe mäüris, fäcilisis sed cöngüe ä, cömmödö eget dölör. Nüllä bibendüm düi ät diäm ültricies lüctüs. In nec sem pellentesqüe velit interdüm semper.
+          </p>
+
+        </section>
       </div>
     );
   }
